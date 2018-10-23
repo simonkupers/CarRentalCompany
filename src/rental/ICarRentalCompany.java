7445252bd33f96125169ed5030603c8288ec4950
Simon Kupers <simon.kupers@gmail.com>
2018-10-23 13:24:40 +0200

    initial commit

---
+++ src/rental/ICarRentalCompany.java
@@ -0,0 +1,24 @@
+package rental;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+public interface ICarRentalCompany extends Remote {
+	
+	public boolean isAvailable(String carTypeName, Date start, Date end) throws RemoteException;
+	
+	public Quote createQuote(ReservationConstraints constraints, String client)
+			throws RemoteException, ReservationException;
+	
+	public Reservation confirmQuote(Quote quote) throws RemoteException, ReservationException;
+	
+	public Set<CarType> getAvailableCarTypes(Date start, Date end) throws RemoteException;
+	
+	public List<Reservation> getReservationsByRenter(String clientName) throws RemoteException;
+	
+	public int getNumberOfReservationsForCarType(String carType) throws RemoteException;
+	
+}
