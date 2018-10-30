b13e078531b863847d9c02a8bba4c5f1c12972f4
tomve <tomthedeveloperr@gmail.com>
2018-10-30 14:50:49 +0100

    SessionManager

--- src/lib/SessionManager.java
+++ src/lib/SessionManager.java
@@ -1,19 +1,44 @@
 package lib;
 
 import java.util.List;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 
 public class SessionManager implements ISessionManager {
-	
-	private List<Session> sessions = new ArrayList<Session>();
-	
-	public void addSession(Session session){
-		sessions.add(session);
+
+	@Override
+	public void createReservationSession(String clientName) {
+		ReservationSession reservationSession = new ReservationSession(clientName);
+		try{
+			Registry registry = LocateRegistry.getRegistry();
+			IReservationSession stub = (IReservationSession) UnicastRemoteObject.exportObject(reservationSession,0);
+			registry.rebind(clientName + "Reservation", stub);
+			
+		}catch(RemoteException e){
+			e.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public void createManagerSession(String clientName, String rentalName) {
+		ManagerSession managerSession = new ManagerSession(clientName, rentalName);
+		try{
+			Registry registry = LocateRegistry.getRegistry();
+			IManagerSession stub = (IManagerSession) UnicastRemoteObject.exportObject(managerSession,0);
+			registry.rebind(clientName + "Manager", stub);
+				
+			}catch(RemoteException e){
+				e.printStackTrace();
+			
+		}
+		
 	}
 	
-	public void removeSession(Session session){
-		sessions.remove(session);
-	}
+	
 	
 	
 	
